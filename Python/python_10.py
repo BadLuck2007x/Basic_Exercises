6364a83c388d22bfca1826ca6d4473bcb05dc66c
Arpan Biswas <arpanbiswas2007x@gmail.com>
2025-11-13 21:18:05 +0530

    Update python_10.py

--- Python/python_10.py
+++ Python/python_10.py
@@ -1,32 +1,31 @@
 # 10.  Count and display the number of vowels, consonants, uppercase, lowercase characters in string.
 
-
 def Count_Alpha(string):
-    Str=str(string).split()
-    vowels = ["A","E","I","O","U"]
-    items={
-        "vowels":0,
-        "consonants":0,
-        "uppercase":0,
-        "lowercase":0,
+    vowels = "AEIOUaeiou"
+    items = {
+        "vowels": 0,
+        "consonants": 0,
+        "uppercase": 0,
+        "lowercase": 0,
     }
-    for i in Str:
-        for j in i:
-            if(j.islower()):
-                items["lowercase"]+=1
-            if(j.isupper()):
-                items["uppercase"]+=1
-            if(j.isdigit):
-                if(j.upper()==(i for i in vowels)):
-                    items["vowels"]+=1
-                else:
-                    items["consonants"]+=1
-    return items
-String=input("Enter the String: ")
-if String:
-    reasult=Count_Alpha(String)
-    print("The number of Vowels is:",reasult["vowels"])
-    print("The number of Consonants is:",reasult["consonants"])
-    print("The number of Uppercase characters  is:",reasult["vowels"])
-    print("The number of Lowercase characters is:",reasult["vowels"])
 
+    for ch in string:
+        if ch.isalpha():
+            if ch in vowels:
+                items["vowels"] += 1
+            else:
+                items["consonants"] += 1
+            if ch.isupper():
+                items["uppercase"] += 1
+            elif ch.islower():
+                items["lowercase"] += 1
+
+    return items
+
+String = input("Enter the String: ")
+if String:
+    result = Count_Alpha(String)
+    print("The number of Vowels is:", result["vowels"])
+    print("The number of Consonants is:", result["consonants"])
+    print("The number of Uppercase characters is:", result["uppercase"])
+    print("The number of Lowercase characters is:", result["lowercase"])
